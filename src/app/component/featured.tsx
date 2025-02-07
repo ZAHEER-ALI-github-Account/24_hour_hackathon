eeb5f136b358192e0e13b34bfae1cf9a9e0e84c8
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2025-02-07 23:36:16 +0500

    Update featured.tsx

--- src/app/component/featured.tsx
+++ src/app/component/featured.tsx
@@ -1,48 +1,10 @@
-// import { client } from "@/sanity/lib/client";
-// import ProductCard from "@/app/component/productCard";
-// import { Product } from "../types/product";
-
-// export default async function featured() {
-//            const resp = await client.fetch(`*[_type=="products"][0..2]{
-//               _id,
-//                 title,
-//                 price,
-//                 description,
-//                 "image":image.asset->url,
-//                 badge,
-//                 inventory,
-//                 _createdAt,
-//                 tags
-//             }`);
-//     return (
-//         <div>
-//         <div className="px-7 py-10 flex flex-col gap-5 ">
-//         <h1 className="font-extrabold text-5xl text-center uppercase tracking-wider text-gray-900">
-//         Feature <span className="text-orange-500">Products</span>
-//       </h1>
-
-//           <div className=" w-full  px-[7vw] grid   grid-cols-1 md:space-x-4 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-//           {resp.map((product : Product) => (
-//                 <ProductCard key={product._id} product={product}/>
-//             ))}
-//           </div>
-//         </div>
-//     </div>
-//     )
-// }
-"use client"; 
-import { useEffect, useState } from "react";
 import { client } from "@/sanity/lib/client";
 import ProductCard from "@/app/component/productCard";
 import { Product } from "../types/product";
 
-export default function Featured() {
-    const [products, setProducts] = useState<Product[]>([]);
-
-    useEffect(() => {
-        async function fetchProducts() {
-            const resp = await client.fetch(`*[_type=="products"][0..2]{
-                _id,
+export default async function FeaturedComponent() {
+           const resp = await client.fetch(`*[_type=="products"][0..2]{
+              _id,
                 title,
                 price,
                 description,
@@ -52,24 +14,62 @@ export default function Featured() {
                 _createdAt,
                 tags
             }`);
-            setProducts(resp);
-        }
-        fetchProducts();
-    }, []);
-
     return (
         <div>
-            <div className="px-7 py-10 flex flex-col gap-5">
-                <h1 className="font-extrabold text-5xl text-center uppercase tracking-wider text-gray-900">
-                    Feature <span className="text-orange-500">Products</span>
-                </h1>
+        <div className="px-7 py-10 flex flex-col gap-5 ">
+        <h1 className="font-extrabold text-5xl text-center uppercase tracking-wider text-gray-900">
+        Feature <span className="text-orange-500">Products</span>
+      </h1>
 
-                <div className="w-full px-[7vw] grid grid-cols-1 md:space-x-4 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-                    {products.map((product) => (
-                        <ProductCard key={product._id} product={product} />
-                    ))}
-                </div>
-            </div>
+          <div className=" w-full  px-[7vw] grid   grid-cols-1 md:space-x-4 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {resp.map((product : Product) => (
+                <ProductCard key={product._id} product={product}/>
+            ))}
+          </div>
         </div>
-    );
+    </div>
+    )
 }
+// "use client"; 
+// import { useEffect, useState } from "react";
+// import { client } from "@/sanity/lib/client";
+// import ProductCard from "@/app/component/productCard";
+// import { Product } from "../types/product";
+
+// export default function Featured() {
+//     const [products, setProducts] = useState<Product[]>([]);
+
+//     useEffect(() => {
+//         async function fetchProducts() {
+//             const resp = await client.fetch(`*[_type=="products"][0..2]{
+//                 _id,
+//                 title,
+//                 price,
+//                 description,
+//                 "image":image.asset->url,
+//                 badge,
+//                 inventory,
+//                 _createdAt,
+//                 tags
+//             }`);
+//             setProducts(resp);
+//         }
+//         fetchProducts();
+//     }, []);
+
+//     return (
+//         <div>
+//             <div className="px-7 py-10 flex flex-col gap-5">
+//                 <h1 className="font-extrabold text-5xl text-center uppercase tracking-wider text-gray-900">
+//                     Feature <span className="text-orange-500">Products</span>
+//                 </h1>
+
+//                 <div className="w-full px-[7vw] grid grid-cols-1 md:space-x-4 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+//                     {products.map((product) => (
+//                         <ProductCard key={product._id} product={product} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
